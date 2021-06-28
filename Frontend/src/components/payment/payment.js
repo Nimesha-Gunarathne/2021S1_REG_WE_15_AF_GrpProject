ac5f4ac47b5f2af1d120b94adb339918f8efa391
Nimesha98 <48381436+Nimesha-Gunarathne@users.noreply.github.com>
2021-06-28 23:02:27 +0530

    New payment page added
    Payment page with new css

--- Frontend/src/components/payment/payment.js
+++ Frontend/src/components/payment/payment.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Select from 'react-select';
+import CheckNullValuesTest from '../../../checkNullValues';
 
 const purposes = [
     { value: 'attendace', label: 'Attendance' },
@@ -44,88 +45,92 @@ class Payment extends Component {
             paymentAmount: this.state.paymentAmount
         }
         console.log('DATA TO SEND : payment', payment);
-        axios.post('http://localhost:8088/payment/insertPayment', payment)
-            .then(response => {
-                alert('Payment Data successfully inserted')
-            })
-            .catch(error => {
-                console.log(error.message);
-                alert(error.message)
-            })
+        var test = CheckNullValuesTest(payment);
+        if (test == "success") {
+            axios.post('http://localhost:3050/payment/insertPayment', payment)
+                .then(response => {
+                    alert('Payment Data successfully inserted')
+                })
+                .catch(error => {
+                    console.log(error.message);
+                    alert(error.message)
+                })
+        }else{
+            console.log("Invalid data set to be passed via axios");
+        }
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="app-wrapper">
-                <div>
-                    <h1 className="title">Payment Gateway</h1>
-                </div>
-                <form onSubmit={this.onSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="payeeName" className="form-label">Payee Name</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="payeeName"
-                            name="payeeName"
-                            value={this.state.payeeName}
-                            onChange={this.onChange}
-                        />
+            <div className="IT19111384_Payment_container">
+                <div className="IT19111384_Payment_app-wrapper">
+                    <div>
+                        <h1 className="IT19111384_Payment_title">Payment Gateway</h1>
                     </div>
-                    <label htmlFor="purpose" className="form-label">Purpose</label>
-                    <Select
-                        options={purposes}
-                        className="basic-select"
-                        // isMulti
-                        onChange={this.onPurposeSelect}
-                    /><br />
-                    <div className="mb-3">
-                        <label htmlFor="payeeEmail" className="form-label">Email</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="payeeEmail"
-                            name="payeeEmail"
-                            value={this.state.payeeEmail}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="cardNumber" className="form-label">Card Number</label>
-                        <input
-                            type="number"
-                            className="form-control"
-                            id="cardNumber"
-                            name="cardNumber"
-                            value={this.state.cardNumber}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="cvv" className="form-label">CVV</label>
-                        <input
-                            type="number"
-                            className="form-control"
-                            id="cvv"
-                            name="cvv"
-                            value={this.state.cvv}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="paymentAmount" className="form-label">Payment Amount</label>
-                        <input
-                            type="number"
-                            className="form-control"
-                            id="paymentAmount"
-                            name="paymentAmount"
-                            value={this.state.paymentAmount}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <button className="confirmPayment" type="submit">Confirm Payment</button>
-                </form>
+                    <form onSubmit={this.onSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="payeeName" className="form-label">Payee Name</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="payeeName"
+                                name="payeeName"
+                                value={this.state.payeeName}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                        <label htmlFor="purpose" className="form-label">Purpose</label>
+                        <Select
+                            options={purposes}
+                            className="basic-select"
+                            onChange={this.onPurposeSelect}
+                        /><br />
+                        <div className="mb-3">
+                            <label htmlFor="payeeEmail" className="form-label">Email</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="payeeEmail"
+                                name="payeeEmail"
+                                value={this.state.payeeEmail}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="cardNumber" className="form-label">Card Number</label>
+                            <input
+                                type="number"
+                                className="form-control"
+                                id="cardNumber"
+                                name="cardNumber"
+                                value={this.state.cardNumber}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="cvv" className="form-label">CVV</label>
+                            <input
+                                type="number"
+                                className="form-control"
+                                id="cvv"
+                                name="cvv"
+                                value={this.state.cvv}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="paymentAmount" className="form-label">Payment Amount</label>
+                            <input
+                                type="number"
+                                className="form-control"
+                                id="paymentAmount"
+                                name="paymentAmount"
+                                value={this.state.paymentAmount}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                        <button className="IT19111384_Payment_confirmPayment" type="submit">Confirm Payment</button>
+                    </form>
                 </div>
             </div>
         )
