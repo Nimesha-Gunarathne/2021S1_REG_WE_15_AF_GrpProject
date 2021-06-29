d26a54e8ca3767d3f4d9fad1bda01c6b1e37db34
sadunika-wasala <kasuni66wasala@gmail.com>
2021-06-29 14:05:17 +0530

    removed app.js and renamed RegistrationServer to Server

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const express = require('express');//import express
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 //env file config
 require('dotenv').config({path: '.env'});
@@ -22,7 +24,21 @@ mongoose.connection.on('error', (err) => {
 require("./src/models/Paper.model");
 require("./src/models/User.model");
 
-const app = require('./app');
+const app = express(); //initialise app with express
+
+var corsOptions = {
+    origin: "http://localhost:1234"
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/src/uploads'));
+
+const routes = require("./src/api/Registration.api");
+
+app.use('/', routes)
 
 //start the server on port 3810
 const server = app.listen(3810,() => {
