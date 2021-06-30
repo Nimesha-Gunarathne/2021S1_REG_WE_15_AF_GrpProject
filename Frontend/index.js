95468af0d1075e84e0d1100a60f330b9db15dafa
sanduni-g <srg19981234@gmail.com>
2021-06-30 09:19:56 +0530

    Notification Frontend

---
+++ Frontend/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+//import './styles.scss';
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
