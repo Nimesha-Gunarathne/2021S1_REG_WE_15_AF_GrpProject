abc9589f09bc0d4e2533414edd928fec5ca987cf
Nimesha98 <48381436+Nimesha-Gunarathne@users.noreply.github.com>
2021-06-30 10:31:12 +0530

    Payment new to merge
    Payment new to merge

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const paymentAPI = require('./src/api/api.payment');
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8088;
-
-const fileAPI = require('./src/api/api.file');
 
 mongoose.connect('mongodb://127.0.0.1:27017/file_upload', {
   useNewUrlParser: true,
@@ -27,7 +27,7 @@ app.route('/').get((req, res) => {
     res.send('Working up to now');
 });
 
-app.use('/file', fileAPI())
+app.use('/payment', paymentAPI());
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on PORT ${PORT}`);
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './AppRouter';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+import './index.css';
 
-
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
-
-
-
+ReactDOM.render(
+<App/>, document.getElementById("root"))
